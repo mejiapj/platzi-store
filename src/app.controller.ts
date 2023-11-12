50da1bcfe44dbb7ea4742993da3cbafb214eb515
mejiapj <mejiapj@gmail.com>
2023-11-12 15:58:59 -0500

    Clase 6/23 -> Introducción a controladores

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'Yo Soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con /sas/';
+  }
 }
