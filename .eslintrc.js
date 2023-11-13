767eedb685ca2257dc192d560f8e7be0e3c4ad24
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-13 13:37:24 -0500

    Clase 14/23 -> Introducción a servicios: crea tu primer servicio

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -22,5 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
